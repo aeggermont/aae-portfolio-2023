6f0a62da4ed8342b7c96e9802cd5a2e459345faf
Antonio A Eggermont <antonio.eggermont@post.harvard.edu>
2023-02-18 15:23:19 -0800

    refactored styles and component for mobile first

--- src/Components/landing_page/landing_page.jsx
+++ src/Components/landing_page/landing_page.jsx
@@ -35,29 +35,3 @@ function LandingPage() {
 }
 
 export default LandingPage;
-
-/*  
-  <div className="container">
-            <div className="main-baner-area">
-                <div className="mobile_wrapper">
-                    <section className="aae-banner">
-                        <div className="row align-items-center">
-                            <img src={bannerPhoto} className="banner-photo" alt="Antonio Aranda Eggermont" />   
-                        </div>
-                        <div className="sphere-shape"></div>
-                        <div className="heading-info">
-                            <span className="heading-primary-intro"> Hello, my name is Antonio </span>
-                            <span className="heading-primary-main"> UX Engineer, Applications Developer & Technologist </span>
-                            <span className="heading-primary-sub"> I design and engineer experiences from inception to completion, and adopt emerging technologies through Human -Centered Design</span>
-                        </div>
-                    </section>
-                </div>
-                <div className="site_menu">
-                    <span> Desktop and Tablets!</span>
-
-                </div>
-            </div>
-        </div>
-
-
-*/
