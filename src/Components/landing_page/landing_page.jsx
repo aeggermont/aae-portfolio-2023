d5a5aaa181e321c92fec65cc425f2b595d48f6a5
Antonio A Eggermont <antonio.eggermont@post.harvard.edu>
2023-02-18 15:22:47 -0800

    refactored styles and component for mobile first

--- src/Components/landing_page/landing_page.jsx
+++ src/Components/landing_page/landing_page.jsx
@@ -8,27 +8,56 @@ import "./landing_page.scss";
 function LandingPage() {
     console.log('THis is the loanding page!');
     return(
-        <div className="container">
-            <section className="aae-banner">
-                <div className="row align-items-center">
-                    <img src={bannerPhoto} className="banner-photo" alt="Antonio Aranda Eggermont" />   
+        <div>
+            <div className="main-baner-area">
+                <div className="aae-banner">
+                    <div class="banner-photo">
+                        <img src={bannerPhoto} width="100%" id="banner-image-dimension" alt="Antonio Aranda Eggermont" />   
+                    </div>
+                    
+                    <div class="heading-info">
+                        <span className="heading-primary-intro"> Hello, my name is Antonio </span>
+                        <span className="heading-primary-main"> UX Engineer, Applications Developer & Technologist </span>
+                        <span className="heading-primary-sub"> I design and engineer experiences from inception to completion, and adopt emerging technologies through Human -Centered Design</span>
+                    </div>
+
+                    <div class="contact-area">
+                        <span> Linkedin </span>
+
+                    </div>
                 </div>
-                <div className="sphere-shape"></div>
-                <div className="heading-info">
-                    <span className="heading-primary-intro"> Hello, my name is Antonio </span>
-                    <span className="heading-primary-main"> UX Engineer, Applications Developer & Technologist </span>
-                    <span className="heading-primary-sub"> I design and engineer experiences from inception to completion, and adopt emerging technologies through Human -Centered Design</span>
-              
-               
-                </div>
-            </section>
+                <div className="sphere-shape"></div> 
+            </div>
+            
         </div>
-      
+ 
     );
 }
 
 export default LandingPage;
 
-/*  <div className="banner-shape">
-                <img src={shapeImg} className="shape_1" alt="shape" />
-            </div> */
+/*  
+  <div className="container">
+            <div className="main-baner-area">
+                <div className="mobile_wrapper">
+                    <section className="aae-banner">
+                        <div className="row align-items-center">
+                            <img src={bannerPhoto} className="banner-photo" alt="Antonio Aranda Eggermont" />   
+                        </div>
+                        <div className="sphere-shape"></div>
+                        <div className="heading-info">
+                            <span className="heading-primary-intro"> Hello, my name is Antonio </span>
+                            <span className="heading-primary-main"> UX Engineer, Applications Developer & Technologist </span>
+                            <span className="heading-primary-sub"> I design and engineer experiences from inception to completion, and adopt emerging technologies through Human -Centered Design</span>
+                        </div>
+                    </section>
+                </div>
+                <div className="site_menu">
+                    <span> Desktop and Tablets!</span>
+
+                </div>
+            </div>
+        </div>
+
+
+*/
