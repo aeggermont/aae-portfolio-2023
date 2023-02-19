024bbd42b48580e745619d313bd9008f16267364
Antonio A Eggermont <antonio.eggermont@post.harvard.edu>
2023-02-18 17:49:30 -0800

    added Background Section in Layout

--- src/Pages/home.jsx
+++ src/Pages/home.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import Layout from "../Layouts";
 import LandingPage from "../Components/landing_page/landing_page";
-
+import BackgroundSection from "../Components/background-section/background-section";
 function Home() {
-    console.log('>>>> Home >>>');
     return (  
         <Layout>
-            <LandingPage/>    
+            <LandingPage/>
+            <BackgroundSection />
         </Layout>
     )
 }
