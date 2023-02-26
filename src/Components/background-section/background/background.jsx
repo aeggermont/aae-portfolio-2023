f9cbb5908ef9ddecc02379d35e514f47a2495831
Antonio A Eggermont <antonio.eggermont@post.harvard.edu>
2023-02-26 11:13:46 -0800

    styling background cards

--- src/Components/background-section/background/background.jsx
+++ src/Components/background-section/background/background.jsx
@@ -5,7 +5,7 @@ import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import CardMedia from '@mui/material/CardMedia';
 import { Button, CardActionArea, CardActions } from '@mui/material';
-
+import './background.scss';
 import UXUIDesignIcon from '../../../assets/images/UXUIDesign-Icon.png'
 
 /*
@@ -23,23 +23,23 @@ export default function BackgroundCard(props) {
 
     const title = props.info.title;
     const img = props.info.img;
+    const dimensions = props.dimensions;
+
     console.log('<<< BackgroundCard ');
-    console.log(props);
+    console.log(dimensions.width);
 
     return (
         <Card
             className={styles.fullHeightCard}
             sx={{ 
-                maxWidth: '230px', 
-                maxHeight: '314px', 
-                minWidth: '150px',
+                width: dimensions.width, 
+                height: dimensions.height,
+            
                 '&:hover': {
                     backgroundColor: 'primary.secondary',
                     opacity: [0.9, 0.8, 0.7],
                 }, 
             }}
-
-            xs={12} sm={12} md={4} lg={4}
             raised='true'
             boxShadow={1}
             
@@ -47,8 +47,7 @@ export default function BackgroundCard(props) {
             <CardActionArea>
                 <CardMedia
                     component="img"
-                    height={'91px'}
-                    width={'91px'}
+                    className="card-media-img"
                     image={img}
                     sx={{ padding: "1em 1em 0 1em", objectFit: "contain" }}
                     alt="My skills in UX / UI Engineering"
@@ -72,5 +71,6 @@ export default function BackgroundCard(props) {
 
 BackgroundCard.prototype = {
     title : PropTypes.string.isRequired,
-    img: PropTypes.object.isRequired
+    img: PropTypes.object.isRequired,
+    dimensions: PropTypes.object.isRequired
 }
