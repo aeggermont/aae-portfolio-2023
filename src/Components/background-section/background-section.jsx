f6aab40ae69670cdb1b0ba2c161c614a5e97585c
Antonio A Eggermont <antonio.eggermont@post.harvard.edu>
2023-02-26 11:11:52 -0800

    added media queries programatically

--- src/Components/background-section/background-section.jsx
+++ src/Components/background-section/background-section.jsx
@@ -7,8 +7,26 @@ import FullStrackDevelopmentIcon from '../../assets/images/FullStrackDevelopment
 import HumanCenteredDesignIcon from '../../assets/images/HumanCenteredDesignIcon.png';
 import SystemsArchitectureIcon from '../../assets/images/SystemsArchitectureIcon.png';
 import './background-section.scss';
+import { useMediaQuery } from "react-responsive";
+import { createTheme, ThemeProvider, styled } from '@mui/material/styles';
+import { useTheme } from '@mui/material/styles';
 
 function BackgroundSection() {
+
+    const sm = 'only screen and (min-width: 576px) and (max-width: 767px)';
+    const md = 'only screen and (min-width: 768px) and (max-width: 991px)';
+    const lg = 'only screen and (min-width: 992px) and (max-width: 1200px)';
+    const laptop = 'only screen and (min-width: 1200px)';
+    const xs = '(max-width: 767px)';
+
+    const isXSmallScr = useMediaQuery({ query: xs });  
+    const isSmallScr  = useMediaQuery({ query: sm });
+    const isMediumScr = useMediaQuery({ query: md });
+    const isLargeScr  = useMediaQuery({ query: lg });
+    const isLaptopScr = useMediaQuery({ query: laptop });
+    const isPortraitScr = useMediaQuery({ query: '(orientation: portrait)' });
+    const isRetinaScr = useMediaQuery({ query: '(min-resolution: 2dppx)' });
+    
     const backgroundInfo = [
         {
             title: 'UX/UI Design & Engineering',
@@ -26,7 +44,56 @@ function BackgroundSection() {
             title: 'Full Stack Development',
             img: FullStrackDevelopmentIcon
         },
-    ]
+    ];
+
+
+    const cardDimensions = function() { 
+        let cardDimensions = {};
+
+        if ( isXSmallScr ) {
+            console.log( ' < isXSmallScr > ');
+            cardDimensions = {
+                width: '200px',
+                height: '210px',
+                isPortraitScr: isPortraitScr
+            };
+    
+        } else if ( isSmallScr ) {
+            console.log( ' < isSmallScr > '); 
+            cardDimensions = {
+                width: '100px',
+                height: '100px',
+                isPortraitScr: isPortraitScr
+            };
+    
+        } else if ( isMediumScr ) {
+            console.log( ' < isMediumScr > '); 
+            cardDimensions = {
+                width: '185px',
+                height: '230px',
+                isPortraitScr: isPortraitScr
+            };
+        } else if ( isLargeScr ) {
+            console.log( ' < isLargeScr > '); 
+            cardDimensions = {
+                width: '100px',
+                height: '100px',
+                isPortraitScr: isPortraitScr
+            };
+        } else if ( isLaptopScr ) {
+            console.log( ' < isLaptopScr > '); cardDimensions = {
+                width: '260px',
+                height: '280px',
+                isPortraitScr: isPortraitScr
+            };
+        
+        };
+    
+        return cardDimensions;
+    };
+    
+
+    console.log(cardDimensions());
 
     return (
         <section>
@@ -38,7 +105,6 @@ function BackgroundSection() {
                         className='section-divider-typography'
                         component="div"> What I do </Typography>
             </div>
-            
             <Box
                 sx={{
                     width: '100%',
@@ -50,11 +116,27 @@ function BackgroundSection() {
                     alignContent: 'flex-start',
                     justifyContent: 'center'
             }}>
-                <BackgroundCard info={backgroundInfo[0]}/>
-                <BackgroundCard info={backgroundInfo[1]}/>
-                <BackgroundCard info={backgroundInfo[2]}/>
-                <BackgroundCard info={backgroundInfo[3]}/>
+
+
+            </Box>    
+            <Box
+                sx={{
+                    width: '100%',
+                    height: '100%',
+                    display: 'flex',
+                    flexWrap: 'wrap',
+                   
+                    gap: 2,
+                    alignContent: 'flex-start',
+                    justifyContent: 'center'
+            }}>
+                
+                <BackgroundCard info={backgroundInfo[0]} dimensions={cardDimensions()}/>
+                <BackgroundCard info={backgroundInfo[1]} dimensions={cardDimensions()}/>
+                <BackgroundCard info={backgroundInfo[2]} dimensions={cardDimensions()}/>
+                <BackgroundCard info={backgroundInfo[3]} dimensions={cardDimensions()}/>
             </Box>
+
         </section>
        
     )
