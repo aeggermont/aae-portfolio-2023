1e70268634d3570decc7c45af8bb7b52a7dc9915
Antonio A Eggermont <antonio.eggermont@post.harvard.edu>
2023-02-28 08:22:05 -0800

    integrated base layout and top level feature pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
-import Home from './Pages/home';
-import ResponsiveTests from './Pages/responsive-tests'
-import './App.scss';
 import {
   BrowserRouter as Router,
   Route
 } from "react-router-dom";
+
+
+import Home from './Pages/home';
+import AboutMe from './Pages/about-me/about-me';
+import MyWork from './Pages/my-work/my-work';
+import Contact from './Pages/contact/contact';
+import ResponsiveTests from './Pages/responsive-tests'
+import './App.scss';
+
 
 function App() {
   return (
@@ -13,6 +19,15 @@ function App() {
       <Router>
         <Route exact path="/">
           <Home />
+        </Route>
+        <Route exact path="/aboutme">
+          <AboutMe />
+        </Route>
+        <Route exact path="/mywork" >
+          <MyWork />
+        </Route>
+        <Route exact path="/contact">
+          <Contact />
         </Route>
         <Route exact path="/screen-test">
           <ResponsiveTests />
