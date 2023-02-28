a8636d78b4e97586f7f1469e3e0ef9b52436b855
Antonio A Eggermont <antonio.eggermont@post.harvard.edu>
2023-02-28 09:07:23 -0800

    dimension adjustments using media queries in componentsç

--- src/Components/main-projects-section/main-projects-section.jsx
+++ src/Components/main-projects-section/main-projects-section.jsx
@@ -6,8 +6,74 @@ import LatestProject from './latest-project/latest-project';
 import Project1 from '../../assets/images/DisneyMagicARTouresIncon.png';
 import Project2 from '../../assets/images/R3XThumbnail.png';
 import Project3 from '../../assets/images/WhereIsPetuniaThumbnail.png';
+import { useMediaQuery } from "react-responsive";
+import Pagination from '@mui/material/Pagination';
+import PaginationItem from '@mui/material/PaginationItem';
+import Stack from '@mui/material/Stack';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+
 
 function MainProjectsSection() {
+
+    const sm = 'only screen and (min-width: 576px) and (max-width: 767px)';
+    const md = 'only screen and (min-width: 768px) and (max-width: 991px)';
+    const lg = 'only screen and (min-width: 992px) and (max-width: 1200px)';
+    const laptop = 'only screen and (min-width: 1200px)';
+    const xs = '(max-width: 767px)';
+
+    const isXSmallScr = useMediaQuery({ query: xs });  
+    const isSmallScr  = useMediaQuery({ query: sm });
+    const isMediumScr = useMediaQuery({ query: md });
+    const isLargeScr  = useMediaQuery({ query: lg });
+    const isLaptopScr = useMediaQuery({ query: laptop });
+    const isPortraitScr = useMediaQuery({ query: '(orientation: portrait)' });
+    const isRetinaScr = useMediaQuery({ query: '(min-resolution: 2dppx)' });
+
+    const cardDimensions = function() { 
+        let cardDimensions = {};
+
+        if ( isXSmallScr ) {
+            console.log( ' < isXSmallScr > ');
+            cardDimensions = {
+                width: '315px',
+                height: '370px',
+                isPortraitScr: isPortraitScr
+            };
+    
+        } else if ( isSmallScr ) {
+            console.log( ' < isSmallScr > '); 
+            cardDimensions = {
+                width: '315px',
+                height: '370px',
+                isPortraitScr: isPortraitScr
+            };
+    
+        } else if ( isMediumScr ) {
+            console.log( ' < isMediumScr > '); 
+            cardDimensions = {
+                width: '315px',
+                height: '370px',
+                isPortraitScr: isPortraitScr
+            };
+        } else if ( isLargeScr ) {
+            console.log( ' < isLargeScr > '); 
+            cardDimensions = {
+                width: '100px',
+                height: '100px',
+                isPortraitScr: isPortraitScr
+            };
+        } else if ( isLaptopScr ) {
+            console.log( ' < isLaptopScr > '); cardDimensions = {
+                width: '260px',
+                height: '280px',
+                isPortraitScr: isPortraitScr
+            };
+        
+        };
+    
+        return cardDimensions;
+    };
 
     const mainProjectsSecion = [
         {
@@ -30,7 +96,7 @@ function MainProjectsSection() {
 
     return (
         <section>
-            <div className='projects-section-divider'>
+            <div className='section-divider'>
                 <Typography
                         align='left'
                         gutterBottom
@@ -50,15 +116,13 @@ function MainProjectsSection() {
                     justifyContent: 'center',
 
                 }}>
-                    <LatestProject info={mainProjectsSecion[0]} />
-                    <LatestProject info={mainProjectsSecion[1]} />
-                    <LatestProject info={mainProjectsSecion[2]} />
+                    <LatestProject info={mainProjectsSecion[0]} dimensions={cardDimensions()} />
             </Box>
 
+            <Stack spacing={2}>
+                <Pagination count={3} color="primary" />
+            </Stack>
         </section>
-
-        
-
     );
 }
 
