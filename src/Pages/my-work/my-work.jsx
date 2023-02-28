d28a1562db3319e133a1abc0dd31b898e9817a40
Antonio A Eggermont <antonio.eggermont@post.harvard.edu>
2023-02-28 09:09:40 -0800

    initial components for my work page

---
+++ src/Pages/my-work/my-work.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from '../../Layouts';
+import Portfolio from './portfolio';
+
+function MyWork() {
+    return (
+        <Layout>
+            <Portfolio/>
+        </Layout>
+    );
+}
+
+export default MyWork;
