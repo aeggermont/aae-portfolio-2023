529404bc6157b21055fb77b023cd508995586c8a
Antonio A Eggermont <antonio.eggermont@post.harvard.edu>
2023-03-18 20:55:17 -0700

    removed use effect

--- src/Components/my-work/my-work.jsx
+++ src/Components/my-work/my-work.jsx
@@ -1,44 +1,8 @@
 import React from 'react';
 import Layout from '../../Layouts';
 import Portfolio from './portfolio';
-import fsReference from '../../firebase';
-import { useEffect, useState } from 'react';
-import { collection, where, getDoc, onSnapshot, orderBy, query, deleteDoc, doc } from "firebase/firestore";;
-
-
-/*
- Portfolio
- Papers
-*/
 
 function MyWork() {
-    const [projects, setProjects] = useState([]);
-    const [headers, setHeaders] = useState({});
-    const [isLoading, setIsLoading] = useState(true);
-    const [hasError, setHasError] = useState(false);
-    const [error, setError] = useState();
-
-    console.log('>>> About to get list of projects <<<');
-
-    useEffect(() => {
-        const docRef = doc(fsReference, 'projects_page', 'projects');
-        getDoc(docRef)
-            .then((doc) => {
-                setProjects(doc.data());
-                setIsLoading(false);
-            })
-            .catch(() => setError('error getting document'));
-    },[]);
-
-    if (isLoading) {
-        return <p>loading...</p>
-    }
-
-    if (hasError) {
-        return <p>Has error!</p>
-    }
-    
-
     return (
         <Layout>
             <Portfolio/>
