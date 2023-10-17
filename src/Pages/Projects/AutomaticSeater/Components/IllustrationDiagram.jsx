b973a75cdf070356436d25fdbdd14907eb13c8ce
Antonio A Eggermont <antonio.eggermont@post.harvard.edu>
2023-10-17 13:11:04 -0700

    implemented reusable Illustration Diagram Component

--- src/Pages/Projects/AutomaticSeater/Components/IllustrationDiagram.jsx
+++ src/Pages/Projects/AutomaticSeater/Components/IllustrationDiagram.jsx
@@ -1,6 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './IllustrationDiagram.scss';
+import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
+import ZoomInButton from '../Images/ZoomInButton.svg';
+import ZoomInButton2x from '../Images/ZoomInButton@2x.svg';
+import ZoomInButton3x from '../Images/ZoomInButton@3x.svg';
+
+import ZoomOutButton from '../Images/ZoomOutButton.svg';
+import ZoomOutButton2x from '../Images/ZoomOutButton@2x.svg';
+import ZoomOutButton3x from '../Images/ZoomOutButton@3x.svg';
+
+import ResetZoomButton from '../Images/ResetZoomButton.svg';
+import ResetZoomButton2x from '../Images/ResetZoomButton@2x.svg';
+import ResetZoomButton3x from '../Images/ResetZoomButton@3x.svg';
 
 function IllustrationDiagram(props) {
     const src = props.src;
@@ -11,17 +23,39 @@ function IllustrationDiagram(props) {
     const screenDevice = props.screenDevice;
     const title = props.title;
     const description = props.description;
-
-    console.log('>>> screenDevice');
-    console.log(screenDevice);
-    
-    console.log('>>> Images');
-    console.log(images);
+    const backgroundColor = props.backgroundColor;
 
     if ( screenDevice.isDesktopOrLaptop ) {
         return (
             <div className='desktop-illlustration-container'>
-                <span className='illustration-title'>  { title} </span>
+                <div className='illustration-title'>  { title} </div>
+                <TransformWrapper
+                    initialScale={1}
+                    initialPositionX={0}
+                    initialPositionY={0}
+                    >
+                    {({ zoomIn, zoomOut, resetTransform, ...rest }) => (
+                        <React.Fragment>
+                        <div className="tools">
+                            <button onClick={() => zoomIn()}>      
+                                <img src={ZoomInButton3x}  alt="test" />
+                            </button>
+                            <button onClick={() => zoomOut()}>     
+                                <img src={ZoomOutButton3x}  alt="test" />  
+                            </button>
+                            <button onClick={() => resetTransform()} >    
+                            <img src={ResetZoomButton3x}  alt="test" />
+                                </button>
+                        </div>
+                        <TransformComponent>
+                            <img src={images[0] }  alt="test" />
+                        
+                        </TransformComponent>
+                        </React.Fragment>
+                    )}
+                 </TransformWrapper>
+
+                {/* 
                 <img
                     src = { images[0] }
                     alt = { alt }
@@ -30,6 +64,7 @@ function IllustrationDiagram(props) {
                         paddingTop: '5px'
                     }} 
                 />
+                */}
                 <span className='text-description'>  { description} </span>
             </div>
            
@@ -38,14 +73,32 @@ function IllustrationDiagram(props) {
         return (
             <div  className='mobile-illlustration-container'>
                 <div className='illustration-title'>  { title} </div>
-                <img
-                    src = { images[2] }
-                    alt = { alt }
-                    style = {{
-                        width: width,
-                        paddingTop: '5px'
-                    }}
-                />
+
+                <TransformWrapper
+                    initialScale={1}
+                    initialPositionX={0}
+                    initialPositionY={0}
+                    >
+                    {({ zoomIn, zoomOut, resetTransform, ...rest }) => (
+                        <React.Fragment>
+                        <div className="tools">
+                            <button onClick={() => zoomIn()}>      
+                                <img src={ZoomInButton}  alt="test" />
+                            </button>
+                            <button onClick={() => zoomOut()}>     
+                                <img src={ZoomOutButton}  alt="test" />  
+                            </button>
+                            <button onClick={() => resetTransform()} >    
+                            <img src={ResetZoomButton}  alt="test" />
+                                </button>
+                        </div>
+                        <TransformComponent>
+                            <img src={images[2] }  alt="test" />
+                        
+                        </TransformComponent>
+                        </React.Fragment>
+                    )}
+                 </TransformWrapper>
                 <span className='text-description'>  { description} </span>
               
              </div>
@@ -53,7 +106,35 @@ function IllustrationDiagram(props) {
     } else if ( screenDevice.isTablet ) {
         return (
             <div className='tablet-illlustration-container'>
-                 <span className='text-title'>  { title} </span>
+                 <div className='text-title'>  { title} </div>
+                 <TransformWrapper
+                    initialScale={1}
+                    initialPositionX={0}
+                    initialPositionY={0}
+                    >
+                    {({ zoomIn, zoomOut, resetTransform, ...rest }) => (
+                        <React.Fragment>
+                        <div className="tools">
+                            <button onClick={() => zoomIn()}>      
+                                <img src={ZoomInButton3x}  alt="test" />
+                            </button>
+                            <button onClick={() => zoomOut()}>     
+                                <img src={ZoomOutButton3x}  alt="test" />  
+                            </button>
+                            <button onClick={() => resetTransform()} >    
+                            <img src={ResetZoomButton3x}  alt="test" />
+                                </button>
+                        </div>
+                        <TransformComponent>
+                            <img src={images[1] }  alt="test" />
+                        
+                        </TransformComponent>
+                        </React.Fragment>
+                    )}
+                 </TransformWrapper>
+
+                 {/*
+
                 <img  
                     src = { images[1] }
                     alt = { alt }
@@ -61,7 +142,7 @@ function IllustrationDiagram(props) {
                         width: width,
                         paddingTop: '120px'
                     }} 
-                />
+                /> */}
                 <span className='text-description'>  { description} </span>
                
             </div>
@@ -79,5 +160,6 @@ IllustrationDiagram.prototype = {
     width: PropTypes.string.isRequired,
     height: PropTypes.string.isRequired,
     images: PropTypes.array.isRequired,
+    backgroundColor: PropTypes.string.isRequired,
     screenDevice: PropTypes.object.isRequired
 }
