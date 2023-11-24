69c80b1338d90e580ddebec616a18c183cd2cdb3
Antonio A Eggermont <antonio.eggermont@post.harvard.edu>
2023-11-23 20:21:50 -0600

    reusable component for Title paragraphs

--- src/Pages/Projects/StoryTeller/Components/TitleParagraph.jsx
+++ src/Pages/Projects/StoryTeller/Components/TitleParagraph.jsx
@@ -2,16 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './TitleParagraph.scss';
 
-const titleParagraphStyle = {
-    fontFamily: 'poppins',
-    fontSize: '20px',
-    fontWeight: '600',
-    color: '#1D2D92',
-    paddingTop: '20px',
-    paddingBottom: '10px',
-    textAlign: 'center'  
-}
-
 function TitleParagraph(props)  {
     const title = props.title;
     const screenDevice = props.screenDevice; 
@@ -20,15 +10,7 @@ function TitleParagraph(props)  {
     if ( screenDevice.isDesktopOrLaptop ) {
         return (
             <>  
-                <div className='desktop-title-container'>
-                    <div className='title-paragraph'> { title }</div>
-                </div>
-            </>
-        )
-    } else if ( screenDevice.isMobile ) {
-        return (
-            <>
-                <div className='mobile-title-container '>
+                <div className='storyteller-desktop-paragraph-titlecontainer'>
                     <div className='title-paragraph'> { title }</div>
                 </div>
             </>
@@ -36,7 +18,15 @@ function TitleParagraph(props)  {
     } else if ( screenDevice.isTablet ) {
         return (
             <>
-                <div className='tablet-title-container '>
+                <div className='storyteller-tablet-paragraph-titlecontainer'>
+                    <div className='title-paragraph'> { title }</div>
+                </div>
+            </>
+        )
+    } else if ( screenDevice.isMobile ) {
+        return (
+            <>
+                <div className='storyteller-mobile-paragraph-titlecontainer'>
                     <div className='title-paragraph'> { title }</div>
                 </div>
             </>
